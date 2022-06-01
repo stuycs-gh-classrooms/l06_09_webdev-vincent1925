f58e4c99c4d7f6a517ef346185c17e5d4492b531
vincent1925 <90664932+vincent1925@users.noreply.github.com>
2022-06-01 14:31:42 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,39 @@
+var rectWidth;
+var rectHeight;
+var x;
+var y;
+var vx;
+var vy;
+
+
 function setup() {
-  // put setup code here
+  createCanvas(600,600);
+  background(0);
+  x = random(width-rectWidth);
+  y = random(height-rectHeight);
+  vx = random(-15, 15);
+  vy = random(-15, 15);
+  rectWidth = 80;
+  rectHeight = 40;
 }
 
 function draw() {
-  // put drawing code here
+  background(0);
+  if (vx==0 ||vy==0) {
+  vx= random(-15, 15);
+  vy= random(-15, 15);
+}
+  movingRectangle ();
+}
+
+function movingRectangle () {
+  rect(x, y, rectWidth, rectHeight);
+  x=vx+x;
+  y=vy+y;
+  if (x>=width-rectWidth || x<=0) {
+    vx=vx*-1;
+  }
+  if (y>=height-rectHeight || y<=0) {
+    vy=vy*-1;
+  }
 }
